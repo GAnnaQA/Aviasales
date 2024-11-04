10613cc5b85a70684ee572b98cde623c8c817518
GAnnaQA <annapiankova4373@gmail.com>
2024-11-04 11:37:30 +0400

    add first test

--- test_tickets_UI.py
+++ test_tickets_UI.py
@@ -1,7 +1,10 @@
 from pages.main_page_tickets import main_page_of_search_tickets
 
 
-def test_test(add_driver):
+def test_choose_popular_destination(add_driver):
     aviasales = main_page_of_search_tickets(add_driver)
     aviasales.choose_country_of_location()
     aviasales.add_coockie()
+    popular_destination = aviasales.choose_popular_destination(3)
+    selected_destination = aviasales.get_destination_value()
+    assert popular_destination == selected_destination
